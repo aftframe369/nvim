fb10410fb58941fab4966191ee2207f42aa7390a
aftframe369 <m4.glinski@gmail.com>
2023-10-12 16:15:04 +0200

    config from home pc

---
+++ lua/setup/surround.lua
@@ -0,0 +1,18 @@
+vim.keymap.set('v', '(', 'S)')
+vim.keymap.set('v', '{', 'S}')
+vim.keymap.set('v', '[', 'S]')
+vim.keymap.set('v', [[']], [[S']])
+vim.keymap.set("v", [["]], [[S"]])
+
+vim.keymap.set('n', 'yss(', 'yss)')
+vim.keymap.set('n', 'yss{', 'yss}')
+vim.keymap.set('n', 'yss[', 'yss]')
+
+vim.keymap.set('n', 'ysiw(', 'ysiw)')
+vim.keymap.set('n', 'ysiw{', 'ysiw}')
+vim.keymap.set('n', 'ysiw[', 'ysiw]')
+
+vim.keymap.set('n', 'cs(', 'cs)')
+vim.keymap.set('n', 'cs{', 'cs}')
+vim.keymap.set('n', 'cs[', 'cs]')
+
