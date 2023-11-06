f8b10f54a566e7d8b4552c8186ab15e82222f8f0
aftframe369 <m4.glinski@gmail.com>
2023-11-06 19:03:09 +0100

    new theme and new kemymaps

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -3,18 +3,19 @@ vim.keymap.set({ 'o', 'n', 'v' }, 'q', 'b')                                     
 vim.keymap.set({ 'o', 'n', 'v' }, 'Q', 'B')                                      -- Q przesuwa do początku SŁOWA na lewo. Bardziej intuicyjne, bo na lewo od 'w'
 vim.keymap.set('i', '<C-H>', '<C-W>')                                            --Ctrl backspace usuwa całe słowo w insert mode
 vim.keymap.set('i', '<C-del>', '.<Esc>ce')                                       --Ctrl delete usuwa słowo w insert mode
-vim.keymap.set('n', '<CR>', 'o')                                                 --Enter in normal mode stars insert in line below
-vim.keymap.set({ 'o', 'n' }, '-', '$')                                           -- replace dolar with - as go to end of line, way better and next to 0 - start of line
+vim.keymap.set('n', '<CR>', 'o')                                                 --Enter in normal mode starts insert in line below
+vim.keymap.set({ 'o', 'n', 'v' }, '-', '$')                                           -- replace dolar with - as go to end of line, way better and next to 0 - start of line
 
-vim.keymap.set({'o', 'n', 'v'}, '<C-t>', '<Esc>:tabnew ')
+vim.keymap.set({'o', 'n', 'v'}, '<C-t>', '<Esc>:tabnew ')    -- ctrl - t makes a new tab (click enter to confirm)
 
 vim.keymap.set('n', 'J', ':m .-2<CR>==')        --use J and K to move lines up and down
 vim.keymap.set('n', 'K', ':m .+1<CR>==')
-vim.keymap.set('v','K', ':m <+1<CR>gv=gv')
-vim.keymap.set('v','J', ':m <-2<CR>gv=gv')
-
-
-
---primegeans moving line up and down in vmode
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set({'o', 'n', 'v'}, 'bb', '%')  --bb jumps to matching brackets
+-- vim.keymap.set({'o', 'n', 'v'}, 'bc', ']*') --bc and Bc jumps to next or previous comments
+-- vim.keymap.set({'o', 'n', 'v'}, 'Bc', '[*')
+vim.keymap.set({'o', 'n', 'v'}, 'bm', ']m') --bm and Bm jumps to next or previous method
+vim.keymap.set({'o', 'n', 'v'}, 'Bm', '[m')
+
