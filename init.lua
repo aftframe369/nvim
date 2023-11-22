ceb5a4b11f6862ad6ee3fe4a9440516bf86cc4ad
aftframe369 <m4.glinski@gmail.com>
2023-11-22 23:11:18 +0100

    coolregex

--- init.lua
+++ init.lua
@@ -23,11 +23,15 @@ require 'setup.lsp'
 -- list = [1, 2, 3, 4]
 -- dict = { opts = pipi }
 -- str = function('your amazing text').method.dict.string(x)
+-- str = function('.')
 vim.keymap.set('n', '<leader>so', ':w<CR>:so<cr>')
 
-vim.keymap.set('n', ']', function()
-  vim.fn.search([[\v['"({[< ]@<=(\w)|^(\w)|(['"\>)}]\.)@<=(\w)]])
+local pattern = [[\v['"({[< ]@<=(\w)|^(\w)|(['"\>)}]\.)@<=(\w)|(['"])@<=([][(){}.,;])(['"])]]
+vim.keymap.set({'n', 'v'}, '<A-w>', function()
+  vim.fn.search(pattern)
 end)
-vim.keymap.set('n', '[', function()
-  vim.fn.search([[\v['"({[< ]@<=(\w)|^(\w)|(['"\>)}]\.)@<=(\w)]], 'b')
+vim.keymap.set({'n', 'v'}, '<A-q>', function()
+  vim.fn.search(pattern, 'b')
 end)
+
+
