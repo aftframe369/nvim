3935343edf5c4459e2b0e98baea49ffa636f848b
aftframe369 <m4.glinski@gmail.com>
2024-10-31 11:14:19 +0100

    added adding lines below and above"

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -112,10 +112,14 @@ remap("n", "<leader>sc", function()
 end
 )
 
-aaremap("n", "zn", "]s", { pipi = "następny błąd"})
+remap("n", "zn", "]s", { desc = "następny błąd"})
 remap("n", "zN", "[s", { desc = "poprzedni błąd"})
 remap("n", "z<leader>", function() vim.fn.feedkeys('z=') end, { desc = "Menu poprawek"})
 remap("n", "z/", function() vim.fn.feedkeys('z=1') end, { desc = "Popraw na pierwszą sugestię"})
 
 -- new recording mapping
 remap("n", "<A-r>", "q", {desc = "recording"})
+
+remap("n", "<leader>o", function() vim.fn.append(vim.fn.line(".")-1, "") end)
+remap("n", "<leader><CR>", function() vim.fn.append(vim.fn.line("."), "") end)
+remap("n", "<leader>O", function() vim.fn.append(vim.fn.line("."), "") end)
