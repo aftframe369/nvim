cc5bd47db7264d3d824a66a528f4ca733899af49
aftframe369 <m4.glinski@gmail.com>
2025-02-08 13:18:48 +0100

    spellchecking

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -133,13 +133,13 @@ remap("n", "<leader>sc", function()
   else
     vim.opt.spell = false
   end
-end
+end,
+  { desc="spellcheck" }
 )
-
 remap("n", "zn", "]s", { desc = "następny błąd"})
 remap("n", "zN", "[s", { desc = "poprzedni błąd"})
 remap("n", "z<leader>", function() vim.fn.feedkeys('z=') end, { desc = "Menu poprawek"})
-remap("n", "z/", function() vim.fn.feedkeys('z=1') end, { desc = "Popraw na pierwszą sugestię"})
+remap("n", "z/", function() vim.fn.feedkeys('1z=') end, { desc = "Popraw na pierwszą sugestię"})
 
 -- new recording mapping
 remap("n", "<A-r>", "q", {desc = "recording"})
