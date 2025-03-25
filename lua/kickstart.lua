cb30930ad54f0499f7d00b805bf66a21bf4588b0
Maciek <m4.glinski@gmail.com>
2025-03-25 14:49:12 +0100

    autokomenda na start vimu

--- lua/kickstart.lua
+++ lua/kickstart.lua
@@ -1,10 +1,4 @@
 return {
-  -- Git related plugins
-  'tpope/vim-fugitive',
-  'tpope/vim-rhubarb',
-
-  -- Detect tabstop and shiftwidth automatically
-  'tpope/vim-sleuth',
 
   -- NOTE: This is where your plugins related to LSP can be installed.
   --  The configuration is done below. Search for lspconfig to find it below.
@@ -37,49 +31,8 @@ return {
   },
 
 
-  {
-    -- Adds git releated signs to the gutter, as well as utilities for managing changes
-    'lewis6991/gitsigns.nvim',
-    opts = {
-      -- See `:help gitsigns.txt`
-      signs = {
-        add = { text = '+' },
-        change = { text = '~' },
-        delete = { text = '_' },
-        topdelete = { text = '‾' },
-        changedelete = { text = '~' },
-      },
-    },
-  },
 
 
 
-  -- "gc" to comment visual regions/lines
-  { 'numToStr/Comment.nvim',         opts = {} },
-
-  -- Fuzzy Finder (files, lsp, etc)
-  { 'nvim-telescope/telescope.nvim', version = '*', dependencies = { 'nvim-lua/plenary.nvim' } },
-
-  -- Fuzzy Finder Algorithm which requires local dependencies to be built.
-  -- Only load if `make` is available. Make sure you have the system
-  -- requirements installed.
-  {
-    'nvim-telescope/telescope-fzf-native.nvim',
-    -- NOTE: If you are having trouble with this installation,
-    --       refer to the README for telescope-fzf-native for more instructions.
-    build = 'make',
-    cond = function()
-      return vim.fn.executable 'make' == 1
-    end,
-  },
-
-  {
-    -- Highlight, edit, and navigate code
-    'nvim-treesitter/nvim-treesitter',
-    dependencies = {
-      'nvim-treesitter/nvim-treesitter-textobjects',
-    },
-    build = ":TSUpdate",
-  }
 
 }
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,5 +1,54 @@
 return {
 	-- amend keymaps
+	-- Git related plugins
+	{ 'tpope/vim-fugitive', },
+	{ 'tpope/vim-rhubarb', },
+
+	-- Adds git releated signs to the gutter, as well as utilities for managing changes
+	{
+		'lewis6991/gitsigns.nvim',
+		opts = {
+			-- See `:help gitsigns.txt`
+			signs = {
+				add = { text = '+' },
+				change = { text = '~' },
+				delete = { text = '_' },
+				topdelete = { text = '‾' },
+				changedelete = { text = '~' },
+			},
+		},
+	},
+
+	-- "gc" to comment visual regions/lines
+	{ 'numToStr/Comment.nvim', opts = {} },
+
+	-- Fuzzy Finder (files, lsp, etc)
+	{
+		'nvim-telescope/telescope.nvim',
+		version = '*',
+		dependencies = { 'nvim-lua/plenary.nvim' }
+	},
+
+	-- Fuzzy Finder Algorithm which requires local dependencies to be built.
+	-- Only load if `make` is available. Make sure you have the system
+	-- requirements installed.
+	{
+		'nvim-telescope/telescope-fzf-native.nvim',
+		build = 'make',
+		cond = function()
+			return vim.fn.executable 'make' == 1
+		end,
+	},
+
+	-- Highlight, edit, and navigate code
+	{
+		'nvim-treesitter/nvim-treesitter',
+		dependencies = {
+			'nvim-treesitter/nvim-treesitter-textobjects',
+		},
+		build = ":TSUpdate",
+	},
+
 	{
 		'anuvyklack/keymap-amend.nvim',
 		init = function()
@@ -16,12 +65,12 @@ return {
 				, { desc = 'delete highlight of search' })
 		end
 	},
-
-	--formatter, DAP, Linter support for Mason
-	{ 'mhartington/formatter.nvim', },
-	{ 'mfussenegger/nvim-dap', },
-	{ 'mfussenegger/nvim-dap-python', },
-	{ 'mfussenegger/nvim-lint', },
+	--
+	--	--formatter, DAP, Linter support for Mason
+	--	{ 'mhartington/formatter.nvim', },
+	--	{ 'mfussenegger/nvim-dap', },
+	--	{ 'mfussenegger/nvim-dap-python', },
+	--	{ 'mfussenegger/nvim-lint', },
 	{ 'nvimtools/none-ls.nvim' },
 	{
 		"zeioth/none-ls-autoload.nvim",
@@ -53,21 +102,22 @@ return {
 	-- See `:help lualine.txt`
 	{
 		'nvim-lualine/lualine.nvim',
-		cond = not vim.g.Chromebook,
+		-- cond = not vim.g.Chromebook,
 		opts = {
 			options = {
-				icons_enabled = true,
+				icons_enabled = false,
 				theme = 'auto',
 				component_separators = { left = '', right = '' },
 				section_separators = { left = '', right = '' },
-				sections = { lualine_c = { "os.date('%a')", 'data', "require'lsp-status'.status()" } }
-				-- component_separators = '/',
-				-- section_separators = '',
+				sections = {
+					lualine_b = {
+						"os.date('%a')",
+						'data',
+						"require'lsp-status'.status()" }
+				}
 			},
 		},
 	},
-
-	{ 'beauwilliams/statusline.lua', cond = vim.g.Chromebook},
 
 	-- surround word/selection in brackets, quotes etc.
 	-- Added better custom mappings
@@ -75,7 +125,7 @@ return {
 	-- Si) to sourround with () in something
 	-- Sd) to delete surrounding ()
 	-- Sc)] to change surrounding () to []
-	{ 'tpope/vim-surround',  init = require('setup.surround') },
+	{ 'tpope/vim-surround',          init = require('setup.surround') },
 
 	-- -- autopair brackets and quotes
 	-- { 'jiangmiao/auto-pairs', },
@@ -90,7 +140,7 @@ return {
 	{ 'ziontee113/color-picker.nvim', opts = {} },
 
 	-- java server
-	{ 'mfussenegger/nvim-jdtls' },
+	--	{ 'mfussenegger/nvim-jdtls' },
 
 	-- Useful plugin to show you pending keybinds.
 	{ 'folke/which-key.nvim',         opts = {} },
@@ -140,18 +190,18 @@ return {
 	-- table mode, :tableMode or <leader>tm to enter table.
 	{ 'dhruvasagar/vim-table-mode', init = require('setup.vim_table_mode') },
 
-	{
-		"iamcco/markdown-preview.nvim",
-		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-		ft = { "markdown" },
-		build = function() vim.fn["mkdp#util#install"]() end,
-		init = function()
-			vim.g.mkdp_preview_options = {
-				mkit = {
-					breaks = true
-				}
-			}
-		end,
-	},
+	--{
+	--	"iamcco/markdown-preview.nvim",
+	--	cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+	--	ft = { "markdown" },
+	--	build = function() vim.fn["mkdp#util#install"]() end,
+	--	init = function()
+	--		vim.g.mkdp_preview_options = {
+	--			mkit = {
+	--				breaks = true
+	--			}
+	--		}
+	--	end,
+	--},
 
 }
